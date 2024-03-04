a599d3ed297f769d56c7f3475ac0dc2da4ab6e3a
NahGoKrazy <mukatinabeel1@gmail.com>
2024-03-03 22:00:13 -0500

    chapter 10

---
+++ constant structure/jss/main.js
@@ -0,0 +1,18 @@
+// let soup = "chicken noodle soup";
+// let isCustomerBanned = false;
+// let soupAccess = isCustomerBanned 
+//     ? "Sorry no soup for you"
+//     : soup 
+//     ? "yes, we have soup for you" : "sorry no soup today"
+//     console.log(soupAccess)
+
+// let testScore = 79;
+// let myGrade = testScore > 89 ? "A" 
+// : testScore > 79 ? "B"
+// :testScore > 69? "D"
+
+
+let playerOne = "rock";
+let compuetr = "rock"
+
+let result = playerOne === computer ? "Tie" : playerOne === "rock" && compueter == "paper" ? "computer wins!"
