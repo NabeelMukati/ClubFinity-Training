30b987adf4d80f2675c75c43169f24903df4cbe9
NahGoKrazy <mukatinabeel1@gmail.com>
2024-03-03 21:25:37 -0500

    chapter 6

--- constant strucuture used/jss/main.js
+++
@@ -1,4 +0,0 @@
-//get numbers from 1-10
-console.log(Math.floor(Math.random() * 10));
-console.log(Math.floor(Math.random() * 10));
-console.log(Math.floor(Math.random() * 10) + 1);z
---
+++ previous files for practice/coding challendge/jss/main.js
@@ -0,0 +1,7 @@
+//planning
+
+let name = prompt()
+
+let i = Math.floor(Math.random() * (name.length + 1));
+console.log(i);
+console.log(name.charAt(i));
