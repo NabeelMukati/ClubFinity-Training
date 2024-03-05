1602ad0a0d19bf082ffd408063f629308dcb4fc3
NahGoKrazy <mukatinabeel1@gmail.com>
2024-03-05 13:38:12 -0500

    chapter 15

---
+++ constant file/jss/main.js
@@ -0,0 +1,26 @@
+const myArray = [];
+c
+
+
+// // add elements to an array 
+// myArray[0] =  "Dave";
+// myArray[1] = 1001;
+// myArray[2] = false;
+// myArray.unshift("hello");
+
+// console.log(myArray.length);
+// const hello = myArray.push(42);
+// console.log(hello);
+
+// myArray.splice(1, 0, 42);
+// console.log(myArray);
+
+
+
+// console.log(myArray);
+
+// //length
+// console.log(myArray.length);
+
+// console.log(myArray[-1]);
+// console.log(myArray[1]);
