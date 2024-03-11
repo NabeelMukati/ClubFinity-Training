1a1cbf3b1e65f0153b781fffa083d032857e02ac
NahGoKrazy <mukatinabeel1@gmail.com>
2024-03-10 23:46:00 -0400

    chapter 17

--- constant file/jss/main.js
+++ constant file/jss/main.js
@@ -1,41 +1,31 @@
-let playGame = confirm("Shall we play rock, paper, or scissors?");
-if (playGame) {
-  //play
-  let playerChoice = prompt("Please enter rock, paper, or scissors.");
-  if (playerChoice) {
-    let playerOne = playerChoice.trim().toLowerCase();
-    if (
-      playerOne === "rock" ||
-      playerOne === "paper" ||
-      playerOne === "scissors"
-    ) {
-      let computerChoice = Math.floor(Math.random() * 3 + 1);
-      let computer =
-        computerChoice === 1
-          ? "rock"
-          : computerChoice === 2
-          ? "paper"
-          : "scissors";
-
-      let result =
-        playerOne === computer
-          ? "Tie game!"
-          : playerOne === "rock" && computer === "paper"
-          ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
-          : playerOne === "paper" && computer === "scissors"
-          ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
-          : playerOne === "scissors" && computer === "rock"
-          ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
-          : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!`;
-      alert(result);
-      let playAgain = confirm("Play Again?");
-      playAgain ? location.reload() : alert("Ok, thanks for playing.");
-    } else {
-      alert("You didn't enter rock, paper, or scissors.");
+const vehicle = {
+    wheels: 4,
+    engine: function(){
+        return "vrrom!";
     }
-  } else {
-    alert("I guess you changed your mind. Maybe next time.");
-  }
-} else {
-  alert("Ok, maybe next time.");
+
 }
+
+const truck = Object.create(vehicle);
+truck.doors = 2;
+console.log(truck);
+console.log(truck.wheels)
+console.log(truck.engine())
+
+
+// const obj = { name: "Nabeel" };
+
+// const another = {
+//     alive: true, 
+//     answer: 42,
+//     hobbies: ["Eat", "sleep", "code"],
+//     beverage:{
+//         morning: "coffee",
+//         afternoon: "iced tea"
+//     },
+//     action: function() {
+//         return `Time for ${this.beverage.morning}`;
+//     }
+
+// };
+// console.log(another.action());
