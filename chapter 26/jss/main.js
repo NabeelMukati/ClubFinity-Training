b2c35a3cb86fbe9f8ee29e623ae93af2e406cf99
NahGoKrazy <mukatinabeel1@gmail.com>
2024-03-28 15:15:18 -0400

    chapter 26

---
+++ chapter 26/jss/main.js
@@ -0,0 +1,12 @@
+const getAllUserEmail = async () =>{
+
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const jsonUserData = await response.json();
+    
+    const userEmailArray = jsonUserData.map(user => {
+        return user.email;
+    });
+
+    console.log(userEmailArray);
+}
+getAllUserEmail();
